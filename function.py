c0cd1bd086205eadfe13dcb548477ff02887cc0e
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-10-08 19:15:47 +0600

    variable decliar done

--- function.py
+++ function.py
@@ -1,11 +1,7 @@
-# Variables
-name = 'salman'
-age = 24
-
 # Function 
 def get_full_name(first, last):
     return f'My name is {first + ' ' + last}.'
 
 # function calling
 full_name = get_full_name("Salman", 'Ahamad')
-print(full_name)
+# print(full_name)
---
+++ variables.py
@@ -0,0 +1,20 @@
+# Variables
+name = 'salman'
+age = 24
+
+a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+b = 12345678910
+
+print('a is of type', type(a))
+print('b is of type', type(b))
+
+c = 4       # c is of type int
+c = "Sally" # c is now of type string
+print(c)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print("Number:", x, y, z)
+
+# Python - Variable Names
