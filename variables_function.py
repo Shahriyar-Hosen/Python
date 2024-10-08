fe633aef0c826ddf1576903426d310340efb991b
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-10-08 19:05:54 +0600

    add comment

--- variables_function.py
+++ variables_function.py
@@ -1,8 +1,11 @@
+# Variables
 name = 'salman'
 age = 24
 
+# Function 
 def get_full_name(first, last):
     return f'My name is {first + ' ' + last}.'
 
+# function calling
 full_name = get_full_name("Salman", 'Ahamad')
 print(full_name)
